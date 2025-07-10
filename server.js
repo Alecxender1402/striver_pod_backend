e7f634e4b34bd64a5c35d9ac90e76b8b0c0500bd
Abhi_Bhingradiya <abhib2706@gmail.com>
2025-07-10 15:17:57 +0530

    update CORS setting

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ const JWT_SECRET = 'jnvoidavianladnvana';
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 // MongoDB connection
